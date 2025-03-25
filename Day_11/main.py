82bdb4bf7f55bae96042efc39af24462b7014ffa
Soham Pawar <96482636+soham7030@users.noreply.github.com>
2025-03-25 13:59:03 +0530

    Today Topic

---
+++ Day_11/main.py
@@ -0,0 +1,10 @@
+str1 ="Hello World 123"
+# isalnum()
+print("is function is numeric :",str1.isalnum())
+# isalpha()
+print("is function is alpha:",str1.isalpha())
+
+str1 ="Hello World 123"
+# len()
+print("len Function :",len(str1))
+
